1083a0a2e3e139bfb49b4528fe8b235541fbe971
Ethan <emh419@hotmail.com>
2025-02-11 17:43:25 -0600

    output

--- scrape.py
+++ scrape.py
@@ -5,15 +5,5 @@ s = Screener("data/skins.txt", None)
 s.read_list()
 
 OUTPUT_FILE = "output/all_skins.csv"
-STEP = 1000
 
-with open(OUTPUT_FILE, 'w', encoding='utf-8') as f:
-    f.write("Item, Buff_Price\n")
-
-i = 0
-while (i+1)*STEP < len(s.scan_list):
-    skins = s.scan_list[i*STEP:(i+1)*STEP]
-    scrape_skins(skins, OUTPUT_FILE)
-    i += 1
-
-scrape_skins(s.scan_list[i*STEP:])
+scrape_skins(s.scan_list, OUTPUT_FILE)
